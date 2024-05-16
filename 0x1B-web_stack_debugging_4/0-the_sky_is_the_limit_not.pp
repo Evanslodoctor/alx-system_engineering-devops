c1a1d200ba9f3d8871d86417c3cbfda302d3590c
Evans Chaun <75381404+Evanslodoctor@users.noreply.github.com>
2024-05-16 13:34:13 +0300

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,14 @@
-# Increases the amount of traffic an Nginx server can handle.
-
 # Increase the ULIMIT of the default file
-exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
+file_line { 'increase_ulimit':
+  ensure => present,
+  path   => '/etc/default/nginx',
+  line   => 'ulimit -n 4096;',
+  match  => '^ulimit -n',
 } ->
 
 # Restart Nginx
-exec { 'nginx-restart':
-  command => 'nginx restart',
-  path    => '/etc/init.d/'
+service { 'nginx':
+  ensure    => running,
+  enable    => true,
+  subscribe => File_line['increase_ulimit'],
 }
